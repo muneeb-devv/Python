773b35c801028d834fc6784dc9c9089820d6efd3
Your Name <your_email@example.com>
2026-05-07 16:42:54 +0500

    add

--- Modulus.py
+++ Modulus.py
@@ -2,3 +2,4 @@ var1 = int(input("enter first number "))
 var2 = int(input("enter second number "))
 Mod = var1 % var2
 print("the result is:", Mod)
+
--- add.py
+++ add.py
@@ -1,4 +1,4 @@
 var1 = int(input("enter first number"))
 var2 = int(input("enter second number"))
 sum = var1 + var2
-print("the result is:", sum)
+print("the result is...............:", sum)
